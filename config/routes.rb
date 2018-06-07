2a8520c1a6060eec7635e6f8c3fffe79b1d49c09
meggangreen <meggan.green@checkr.com>
2018-06-07 11:54:10 -0700

    add home and help pages; update / route

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   resources :professors
   resources :students
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'students#index'
+  root 'static_pages#home'
 end
